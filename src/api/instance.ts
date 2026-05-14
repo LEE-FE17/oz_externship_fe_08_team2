8fb674234ee4dffa7a1fc3b678b48c9a8a956369
LEE-FE17 <mi6354828@gmail.com>
2026-05-14 14:05:50 +0900

    fix: 상세페이지 오류 수정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -6,7 +6,7 @@ const api = axios.create({
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc4ODIwMDI1LCJpYXQiOjE3Nzg3MzM2MjUsImp0aSI6IjM1NTdiMjY0NWY1ODRkMTJiODc1MTY5NDVmZGY4YWJiIiwidXNlcl9pZCI6M30.79Q-mzI3gcsG8B33MPTeqlSISTSAZf8iZ6_YsGQT7l0`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc4ODIwNTMzLCJpYXQiOjE3Nzg3MzQxMzMsImp0aSI6IjZiMDY2NjBlYzJkODQzODk5ZjQyYjAzNzQwNDJjYmYyIiwidXNlcl9pZCI6M30.XaD0d7vyDZ6czyK5x0bYbnp3L9HK_p8IbrXwPCiDcuY`,
   },
   withCredentials: true,
 })
