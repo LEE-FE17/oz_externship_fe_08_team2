b8d77234814bc0bc3d3fcefc857d67af743d9d5b
최민제 <alswpchl0818@gmail.com>
2026-05-15 14:13:18 +0900

    fix: 하드코딩된 JWT 인증 헤더 제거

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -6,7 +6,6 @@ const api = axios.create({
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc4ODM0MjM1LCJpYXQiOjE3Nzg3NDc4MzUsImp0aSI6IjE3NTcwMzU2NDY0NDRiZTViYzJhYTkwMTBjYmZjN2FmIiwidXNlcl9pZCI6Mjl9.c7uVVm0RNpHmCyX7z7uQ2np4D-1PKGAZbJ0qAcZHlnQ`,
   },
   withCredentials: true,
 })
